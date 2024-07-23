91d4a479412161c1351ed476df5c522059fd90b9
“C.K” <dripcoke24@gmail.com>
2024-07-23 11:51:28 +0900

    ローカルにdark option保存

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,25 +1,33 @@
 'use client'
 
 import { Button } from '@mui/material'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const ThemeSwitcher = () => {
   const [mode, setMode] = useState('light')
+
+  useEffect(() => {
+    // 初期テーマをlocalStorageから取得
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme) {
+      setMode(savedTheme)
+      document.documentElement.classList.add(savedTheme)
+    }
+  }, [])
 
   const toggleTheme = () => {
     // 初期テーマをlocalStorageから取得
     setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
     document.documentElement.classList.toggle('dark', mode === 'light')
+    localStorage.setItem('theme', mode)
   }
 
   return (
-    <div className="p-4 bg-white dark:bg-black text-black dark:text-white min-h-screen">
-      <h1 className="text-2xl">MUIとTailwindでのダークモード実装例</h1>
+    <>
       <Button onClick={toggleTheme} variant="contained" className="mt-4">
         {mode === 'light' ? 'ダークモードに切り替え' : 'ライトモードに切り替え'}
       </Button>
-      {/* 他のコンテンツ */}
-    </div>
+    </>
   )
 }
 
