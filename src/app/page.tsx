35b6237a726774fab71e09f544387e939f2899d5
“C.K” <dripcoke24@gmail.com>
2024-07-23 09:25:41 +0900

    ダークモード実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import ThemeSwitcher from '@/components/ThemeSwitcher'
 
 export default function Home() {
   return (
     <>
+      <div>
+        <ThemeSwitcher />
+      </div>
     </>
-  );
+  )
 }
---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { Button, createTheme, CssBaseline } from '@mui/material'
+import { palette } from '@mui/system'
+import { useMemo, useState } from 'react'
+import {
+  ThemeProvider as EmotionThemeProvider,
+  ThemeProvider,
+} from '@emotion/react'
+
+const ThemeSwitcher = () => {
+  const [mode, setMode] = useState('light')
+
+  const toggleTheme = () => {
+    // 初期テーマをlocalStorageから取得
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+    document.documentElement.classList.toggle('dark', mode === 'light')
+  }
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  )
+
+  return (
+    <EmotionThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="p-4 bg-white dark:bg-black text-black dark:text-white min-h-screen">
+          <h1 className="text-2xl">MUIとTailwindでのダークモード実装例</h1>
+          <Button onClick={toggleTheme} variant="contained" className="mt-4">
+            {mode === 'light'
+              ? 'ダークモードに切り替え'
+              : 'ライトモードに切り替え'}
+          </Button>
+          {/* 他のコンテンツ */}
+        </div>
+      </ThemeProvider>
+    </EmotionThemeProvider>
+  )
+}
+
+export default ThemeSwitcher
