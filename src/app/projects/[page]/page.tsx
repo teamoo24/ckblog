41890abd82970f8e00bdfeac55f4692d1585f4cd
“C.K” <dripcoke24@gmail.com>
2024-08-02 16:06:25 +0900

    dynamic route対応

---
+++ src/app/projects/[page]/page.tsx
@@ -0,0 +1,72 @@
+import { Metadata } from "next";
+import React from "react";
+import Repository from "@/types/Repository";
+import fetchRepositories from "@/libs/fetchRepositories";
+
+export const metadata: Metadata = {
+  title: "プロジェクト一覧",
+};
+
+const ProjectsPage = async ({ params }: { params: { page: string } }) => {
+  const page = parseInt(params.page, 10); // 現在のページ番号
+  const perPage = 20; // 1ページあたりのリポジトリ数
+
+  let repositories: Repository[] = [];
+  let error: string | null = null;
+
+  try {
+    repositories = await fetchRepositories(page, perPage);
+  } catch (err) {
+    error = (err as Error).message;
+  }
+
+  return (
+    <section className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">プロジェクト一覧</h1>
+      {error ? (
+        <p className="text-red-500">エラー: {error}</p>
+      ) : (
+        <>
+          <ul className="list-disc list-inside ml-4">
+            {repositories.length > 0 ? (
+              repositories.map((repo) => (
+                <li key={repo.name} className="mb-2">
+                  <a
+                    href={repo.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline"
+                  >
+                    {repo.name}
+                  </a>
+                  <p className="text-gray-600">{repo.description}</p>
+                </li>
+              ))
+            ) : (
+              <li>リポジトリが見つかりませんでした。</li>
+            )}
+          </ul>
+
+          <div className="mt-4 flex justify-between">
+            {page > 1 && (
+              <a
+                href={`/projects/${page - 1}`}
+                className="text-blue-500 hover:underline"
+              >
+                前のページ
+              </a>
+            )}
+            <a
+              href={`/projects/${page + 1}`}
+              className="text-blue-500 hover:underline"
+            >
+              次のページ
+            </a>
+          </div>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default ProjectsPage;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,71 +1,36 @@
-import React from "react";
-import Repository from "@/types/Repository";
 import fetchRepositories from "@/libs/fetchRepositories";
+import React from "react";
 
-const ProjectsPage = async ({
-  searchParams,
-}: {
-  searchParams: { page?: string };
-}) => {
-  const page = parseInt(searchParams.page || "1", 10); // 現在のページ番号
-  const perPage = 20; // 1ページあたりのリポジトリ数
-
-  let repositories: Repository[] = [];
-  let error: string | null = null;
-
-  try {
-    repositories = await fetchRepositories(page, perPage);
-  } catch (err) {
-    error = (err as Error).message;
-  }
+const ProjectsPageDefault = async () => {
+  const defaultPage = 1;
+  const perPage = 20;
+  const repositories = await fetchRepositories(defaultPage, perPage);
 
   return (
     <section className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">プロジェクト一覧</h1>
-      {error ? (
-        <p className="text-red-500">エラー: {error}</p>
-      ) : (
-        <>
-          <ul className="list-disc list-inside ml-4">
-            {repositories.length > 0 ? (
-              repositories.map((repo) => (
-                <li key={repo.name} className="mb-2">
-                  <a
-                    href={repo.html_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 hover:underline"
-                  >
-                    {repo.name}
-                  </a>
-                  <p className="text-gray-600">{repo.description}</p>
-                </li>
-              ))
-            ) : (
-              <li>リポジトリが見つかりませんでした。</li>
-            )}
-          </ul>
-
-          <div className="mt-4 flex justify-between">
-            {page > 1 && (
-              <a
-                href={`?page=${page - 1}`}
-                className="text-blue-500 hover:underline"
-              >
-                前のページ
-              </a>
-            )}
+      <ul className="list-disc list-inside ml-4">
+        {repositories.map((repo) => (
+          <li key={repo.name} className="mb-2">
             <a
-              href={`?page=${page + 1}`}
+              href={repo.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
               className="text-blue-500 hover:underline"
             >
-              次のページ
+              {repo.name}
             </a>
-          </div>
-        </>
-      )}
+            <p className="text-gray-600">{repo.description}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="mt-4">
+        <a href="/projects/2" className="text-blue-500 hover:underline">
+          次のページ
+        </a>
+      </div>
     </section>
   );
 };
 
-export default ProjectsPage;
+export default ProjectsPageDefault;
